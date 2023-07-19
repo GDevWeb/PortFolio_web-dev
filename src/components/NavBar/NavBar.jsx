58e018b856e63ca9a1b748f5e1a2fcda94f4cee7
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-07-19 21:45:16 +0200

    merge develop+

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './Navbar.scss';
+import './NavBar.scss';
 import barsIcon from '../../assets/icons/bars.svg'
 
 
